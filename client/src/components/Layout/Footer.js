2d3deae975c1d5df573454fd656a607e13fe1dda
Gautam Singh <gautamsingh@Gautams-MacBook-Air.local>
2024-08-25 20:52:09 +0530

    first commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-light p-4">
+      <h6 className="text-center text-white">
+        Made By - Gautam Singh
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
